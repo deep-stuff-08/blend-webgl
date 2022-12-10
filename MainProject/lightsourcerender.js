fdfcc0397cb29e0e33fcd599a6cd66784299a13b
Deep Lalwani <deepthesmartest@gmail.com>
2022-12-10 19:58:15 +0530

    LightSourceRenderer

---
+++ MainProject/lightsourcerender.js
@@ -0,0 +1,37 @@
+var progLightSource = {
+	program: null,
+	cube: null,
+	uniforms: {
+		pMat: null,
+		vMat: null,
+		lightPos: null,
+		lightColor: null
+	}
+}
+
+function setupProgramForLightSourceRendererDeep() {
+	vertShader = createShader('shaders/lightsource.vert', gl.VERTEX_SHADER)
+	fragShader = createShader('shaders/lightsource.frag', gl.FRAGMENT_SHADER)
+	progLightSource.program = createProgram([vertShader, fragShader])
+	deleteShader(vertShader)
+	deleteShader(fragShader)
+
+	progLightSource.uniforms.pMat = gl.getUniformLocation(progLightSource.program, "pMat")
+	progLightSource.uniforms.vMat = gl.getUniformLocation(progLightSource.program, "vMat")
+	progLightSource.uniforms.lightPos = gl.getUniformLocation(progLightSource.program, "lightPos")
+	progLightSource.uniforms.lightColor = gl.getUniformLocation(progLightSource.program, "lightColor")
+}
+
+function initForLightSourceRendererDeep() {
+	progLightSource.cube = initCubeForShapesDeep()
+}
+
+function renderLightSourceDeep(perspectiveMatrix, viewMatrix, lightPos, lightColor) {
+	gl.useProgram(progLightSource.program)
+	gl.uniformMatrix4fv(progLightSource.uniforms.pMat, false, perspectiveMatrix)
+	gl.uniformMatrix4fv(progLightSource.uniforms.vMat, false, viewMatrix)
+	gl.uniform3fv(progLightSource.uniforms.lightPos, lightPos)
+	gl.uniform3fv(progLightSource.uniforms.lightColor, lightColor)
+	renderCubeForShapesDeep(progLightSource.cube)
+	gl.useProgram(null)
+}
