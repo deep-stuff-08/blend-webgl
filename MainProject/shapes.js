51507e49fae6ddd42d096e6358016acd00535555
Deep Lalwani <deepthesmartest@gmail.com>
2022-12-11 01:21:45 +0530

    Integrated Table

--- MainProject/shapes.js
+++ MainProject/shapes.js
@@ -138,9 +138,74 @@ function initCubeForShapesDeep() {
 	return new dshapes(vao, 36)
 }
 
+function initCylinderForShapesDeep(slices) {
+	var vertexData = []
+	for(var j = 0; j <= slices; j++) {
+		var theta = 2.0 * Math.PI * j / slices
+		vertexData.push(Math.sin(theta))
+		vertexData.push(1.0)
+		vertexData.push(Math.cos(theta))
+		
+		vertexData.push(Math.sin(theta))
+		vertexData.push(0.0)
+		vertexData.push(Math.cos(theta))
+
+		vertexData.push(j / slices)
+		vertexData.push(1.0)
+	
+		vertexData.push(Math.sin(theta))
+		vertexData.push(-1.0)
+		vertexData.push(Math.cos(theta))
+		
+		vertexData.push(Math.sin(theta))
+		vertexData.push(0.0)
+		vertexData.push(Math.cos(theta))
+
+		vertexData.push(j / slices)
+		vertexData.push(0.0)
+	}
+	var elements = []
+	for(var j = 0; j < slices; j++) {
+		elements.push(j * 2)
+		elements.push(j * 2 + 1)
+		elements.push((j + 1) * 2)
+		elements.push(j * 2 + 1)
+		elements.push((j + 1) * 2)
+		elements.push((j + 1) * 2 + 1)
+	}
+	var elementIndices = Uint16Array.from(elements)
+	var vertexArray = Float32Array.from(vertexData)
+
+	var vao = gl.createVertexArray()
+	gl.bindVertexArray(vao)
+
+	var vbo = gl.createBuffer()
+	gl.bindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.bufferData(gl.ARRAY_BUFFER, vertexArray, gl.STATIC_DRAW)
+	gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 8 * 4, 0)
+	gl.enableVertexAttribArray(0)
+	gl.vertexAttribPointer(1, 3, gl.FLOAT, false, 8 * 4, 3 * 4)
+	gl.enableVertexAttribArray(1)
+	gl.vertexAttribPointer(2, 2, gl.FLOAT, false, 8 * 4, 6 * 4)
+	gl.enableVertexAttribArray(2)
+	
+	var eabo = gl.createBuffer()
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, eabo)
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, elementIndices, gl.STATIC_DRAW)
+
+	gl.bindVertexArray(null)
+	return new dshapes(vao, elementIndices.length)
+}
+
 function renderSphereForShapesDeep(sphereObj) {
 	gl.bindVertexArray(sphereObj.vao)
 	gl.drawElements(gl.TRIANGLES, sphereObj.count, gl.UNSIGNED_SHORT, 0)
+	gl.bindVertexArray(null)
+}
+
+function renderCylinderForShapesDeep(cylinderObj) {
+	gl.bindVertexArray(cylinderObj.vao)
+	gl.drawElements(gl.TRIANGLES, cylinderObj.count, gl.UNSIGNED_SHORT, 0)
 	gl.bindVertexArray(null)
 }
 
