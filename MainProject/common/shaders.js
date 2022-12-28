1157ce4cb99470811a691c847c5788f41b4eab11
Deep Lalwani <deepthesmartest@gmail.com>
2022-12-28 11:27:48 +0530

    Pre mess up commit

--- MainProject/common/shaders.js
+++ MainProject/common/shaders.js
@@ -30,10 +30,28 @@ var progPhongLightWithTextureForModel = {
 	}
 }
 
+var progCompleteLight = {
+	program: null,
+	uniforms: {
+		pMat: null,
+		vMat: null,
+		mMat: null,
+		texMat: null,
+		viewPos: null,
+		lightPos: null,
+		diffuseTextureSampler: null,
+		isInvertNormals: null,
+		isLight: null,
+		isTexture: null,
+		isBlend: null
+	}
+}
+
 function setupCommonPrograms() {
 	//Phong Light with Texture Support
-	var vertShader = createShader('common/shaders/phonglighttex.vert', gl.VERTEX_SHADER)
-	var fragShader = createShader('common/shaders/phonglighttex.frag', gl.FRAGMENT_SHADER)
+	var vertShader, fragShader
+	vertShader = createShader('common/shaders/phonglighttex.vert', gl.VERTEX_SHADER)
+	fragShader = createShader('common/shaders/phonglighttex.frag', gl.FRAGMENT_SHADER)
 	progPhongLightWithTexture.program = createProgram([vertShader, fragShader])
 	deleteShader(vertShader)
 	deleteShader(fragShader)
@@ -66,4 +84,69 @@ function setupCommonPrograms() {
 	progPhongLightWithTextureForModel.uniforms.isLight = gl.getUniformLocation(progPhongLightWithTextureForModel.program, "isLight")
 	progPhongLightWithTextureForModel.uniforms.isTexture = gl.getUniformLocation(progPhongLightWithTextureForModel.program, "isTexture")
 	progPhongLightWithTextureForModel.uniforms.isBlend = gl.getUniformLocation(progPhongLightWithTextureForModel.program, "isBlend")
+
+	vertShader = createShader('common/shaders/phonglighttex.vert', gl.VERTEX_SHADER)
+	fragShader = createShader('common/shaders/phonglighttex.frag', gl.FRAGMENT_SHADER)
+	progCompleteLight.program = createProgram([vertShader, fragShader])
+	deleteShader(vertShader)
+	deleteShader(fragShader)
+
+	progCompleteLight.uniforms.pMat = gl.getUniformLocation(progCompleteLight.program, "pMat")
+	progCompleteLight.uniforms.vMat = gl.getUniformLocation(progCompleteLight.program, "vMat")
+	progCompleteLight.uniforms.mMat = gl.getUniformLocation(progCompleteLight.program, "mMat")
+	progCompleteLight.uniforms.texMat = gl.getUniformLocation(progCompleteLight.program, "texMat")
+	progCompleteLight.uniforms.lightPos = gl.getUniformLocation(progCompleteLight.program, "lightPos")
+	progCompleteLight.uniforms.viewPos = gl.getUniformLocation(progCompleteLight.program, "viewPos")
+	progCompleteLight.uniforms.diffuseTextureSampler = gl.getUniformLocation(progCompleteLight.program, "samplerDiffuse")
+	progCompleteLight.uniforms.isInvertNormals = gl.getUniformLocation(progCompleteLight.program, "isInvertNormal")
+	progCompleteLight.uniforms.isLight = gl.getUniformLocation(progCompleteLight.program, "isLight")
+	progCompleteLight.uniforms.isTexture = gl.getUniformLocation(progCompleteLight.program, "isTexture")
+	progCompleteLight.uniforms.isBlend = gl.getUniformLocation(progCompleteLight.program, "isBlend")
+	gl.useProgram(progCompleteLight.program)
+	resetCompleteLight()
+	gl.useProgram(null)
+}
+
+function resetCompleteLight() {
+	gl.uniformMatrix4fv(progCompleteLight.uniforms.pMat, false, mat4.create())
+	gl.uniformMatrix4fv(progCompleteLight.uniforms.vMat, false, mat4.create())
+	gl.uniformMatrix4fv(progCompleteLight.uniforms.mMat, false, mat4.create())
+	gl.uniformMatrix2fv(progCompleteLight.uniforms.texMat, false, mat2.create())
+	gl.uniform3f(progCompleteLight.uniforms.lightPos, 0.0, 0.0, 0.0)
+	gl.uniform3f(progCompleteLight.uniforms.viewPos, 0.0, 0.0, 0.0)
+	gl.uniform1i(progCompleteLight.uniforms.diffuseTextureSampler, 0)
+	gl.uniform1i(progCompleteLight.uniforms.isBlend, 0)
+	gl.uniform1i(progCompleteLight.uniforms.isInvertNormals, 0)
+	gl.uniform1i(progCompleteLight.uniforms.isLight, 0)
+	gl.uniform1i(progCompleteLight.uniforms.isTexture, 0)
+}
+
+function setPerojectionAndViewCompleteLight(perspectiveMatrix, viewMatrix, viewPos) {
+	gl.uniformMatrix4fv(progCompleteLight.uniforms.pMat, false, perspectiveMatrix)
+	gl.uniformMatrix4fv(progCompleteLight.uniforms.vMat, false, viewMatrix)
+	gl.uniform3fv(progCompleteLight.uniforms.viewPos, viewPos)
+}
+
+function setModelMatrix(modelMatrix) {
+	gl.uniformMatrix4fv(progCompleteLight.uniforms.mMat, false, modelMatrix)
+}
+
+function setTextureMatrix(textureMatrix) {
+	gl.uniformMatrix2fv(progCompleteLight.uniforms.texMat, false, textureMatrix)
+}
+
+function setTextureSamplers(diffuseTexture) {
+	gl.uniform1i(progCompleteLight.uniforms.isTexture, 1)
+	gl.uniform1i(progCompleteLight.uniforms.diffuseTextureSampler, diffuseTexture)
+}
+
+function setFlags(isInvertNormals, isBlend, isTexture, isLight) {
+	gl.uniform1i(progCompleteLight.uniforms.isBlend, isBlend)
+	gl.uniform1i(progCompleteLight.uniforms.isInvertNormals, isInvertNormals)
+	gl.uniform1i(progCompleteLight.uniforms.isLight, isLight)
+	gl.uniform1i(progCompleteLight.uniforms.isTexture, isTexture)
+}
+
+function addLight(lightPos, lightColor) {
+	gl.uniform3fv(progCompleteLight.uniforms.lightPos, lightPos)
 }
